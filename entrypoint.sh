e8c27f730a017623ac74a8c0678c9e0b78b0db7c
juanfcarrillo <juanfi444@outlook.com>
2025-09-02 21:59:16 -0500

    feat: activated virtual env

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,8 @@
 
 echo "Starting API server..."
 
+# Activate the virtual environment
+source /workspace/venv/bin/activate
+
 # Start the API server
 /workspace/venv/bin/python /workspace/Hunyuan3D-2.1/api_server.py
